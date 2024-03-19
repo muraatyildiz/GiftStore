355692342fc005f2e310511f026695556c965b93
murat yildiz <mrtyldz118@gmail.com>
2024-03-19 06:45:55 +0000

    updates

--- Backend/app.py
+++ Backend/app.py
@@ -27,4 +27,5 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	# app.run(debug=True)
+	app.run(host='0.0.0.0',port='8080')
