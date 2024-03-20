99419d528d200a662bdc844e5c9f6b117bf9bcd1
murat yildiz <mrtyldz118@gmail.com>
2024-03-20 12:13:01 +0000

    home page updates and backend connections

--- Frontend/layouts/default.vue
+++ Frontend/layouts/default.vue
@@ -1,119 +1,132 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app id="inspire">
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
       app
+      color="primary"
+      dark
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
+
+      <v-toolbar-title style="width: 350px">
+        <a href="/" class="white--text" style="text-decoration: none"
+          ><v-icon>mdi-truck</v-icon>&nbsp;ShipIT</a
+        >
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        v-model="search"
+        class="hidden-sm-and-down pl-10 ml-4"
+      />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <nuxt-link to="/user">
+        <v-btn icon>
+          <v-icon>mdi-account-circle</v-icon>
+        </v-btn>
+      </nuxt-link>
+      <nuxt-link to="/wishlist">
+      <v-btn icon>
+        <v-badge content="2" value="2" color="green" overlap>
+          <v-icon>mdi-bell</v-icon>
+        </v-badge>
       </v-btn>
+    </nuxt-link>
+    <nuxt-link to="/cart">
+      <v-btn icon>
+        <v-badge content="2" value="2" color="green" overlap>
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
+    </nuxt-link>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-content>
+      <v-bottom-navigation :value="activeBtn" color="primary" horizontal>
+        <a href="/" class="v-btn">
+          <span>Home</span>
+        </a>
+        <v-menu open-on-hover offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on">
+              <span>Shop</span>
+            </v-btn>
+          </template>
+          <v-card class="mx-auto" max-width="344" outlined>
+            <v-list-item v-for="(item, index) in items" :key="index" href="/">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-card>
+        </v-menu>
+        <a href="/" class="v-btn">
+          <span>Products</span>
+        </a>
+        <v-btn href="/">
+          <span>Collections</span>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-content>
+
+    <Nuxt />
+
+    <v-footer :padless="true">
+      <v-card flat tile width="100%" class="secondary white--text text-center">
+        <v-card-text>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-home</v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-email</v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-calendar</v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-delete</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
+          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
+          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>ShipIT</strong>
+        </v-card-text>
+      </v-card>
     </v-footer>
   </v-app>
 </template>
-
 <script>
 export default {
-  name: 'DefaultLayout',
-  middleware: ["session-control", "auth"],
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+        { title: "T-Shirts" },
+        { title: "Jackets" },
+        { title: "Shirts" },
+        { title: "Jeans" },
+        { title: "Shoes" },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      activeBtn: 1,
+      search:''
+    };
+  },
+  watch: {
+    'search'() {
+          this.$router.push({path: './', query: { search: this.search }})
     }
-  }
-}
+  },
+};
 </script>
--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -9,6 +9,7 @@
             <template>
               <v-treeview
                 :items="items"
+                :open="[1, 2]"
                 :selected-color="'#fff'"
                 activatable
                 @update:active="selectCategory"
@@ -25,6 +26,7 @@
               :height="10"
               class="align-center"
               dense
+              @change="handleRangeChange"
             ></v-range-slider>
             <v-row class="pa-2" dense>
               <v-col cols="12" sm="5">
@@ -49,7 +51,7 @@
                 ></v-text-field>
               </v-col>
             </v-row>
-            <v-divider></v-divider>
+            <!-- <v-divider></v-divider>
             <v-card-title class="pb-0">Customer Rating</v-card-title>
             <v-container class="pt-0" fluid>
               <v-checkbox
@@ -77,8 +79,8 @@
                 dense
               ></v-checkbox>
             </v-container>
-            <v-divider></v-divider>
-            <v-card-title class="pb-0">Brand</v-card-title>
+            <v-divider></v-divider> -->
+            <!-- <v-card-title class="pb-0">Brand</v-card-title>
             <v-container class="pt-0" fluid>
               <v-checkbox label="XS" hide-details dense></v-checkbox>
               <v-checkbox label="S" hide-details dense></v-checkbox>
@@ -87,7 +89,7 @@
               <v-checkbox label="XL" hide-details dense></v-checkbox>
               <v-checkbox label="XXL" hide-details dense></v-checkbox>
               <v-checkbox label="XXXL" hide-details dense></v-checkbox>
-            </v-container>
+            </v-container> -->
           </v-card>
         </div>
         <div class="col-md-9 col-sm-9 col-xs-12">
@@ -105,7 +107,7 @@
             <v-col cols="12" sm="4">
               <v-select
                 class="pa-0"
-                v-model="select"
+                v-model="selectedSort"
                 :items="options"
                 style="margin-bottom: -20px"
                 outlined
@@ -183,11 +185,10 @@
 export default {
   data: () => ({
     range: [0, 10000],
-    select: "Popularity",
+    selectedSort: "Relevance",
     options: [
-      "Default",
-      "Popularity",
       "Relevance",
+      "Popularity",
       "Price: Low to High",
       "Price: High to Low",
     ],
@@ -232,21 +233,47 @@ export default {
       },
     ],
     products: [],
-    filteredProducts:[],
+    filteredProducts: [],
     pages: null,
     totalPoruducts: null,
     search: "",
+    category: "",
   }),
   watch: {
     "$route.query"() {
       if (this.$route.query.search && this.$route.query.search.length > 2) {
-        this.getProductList(1, this.$route.query.search);
+        this.getProductList(1, this.$route.query.search, this.category);
       } else {
-        this.getProductList(1, this.search);
+        this.getProductList(1, this.search, this.category);
+      }
+    },
+    selectedSort(newValue, oldValue) {
+      switch (newValue) {
+        case "Relevance":
+          this.products.sort((a, b) =>a._id.localeCompare(b._id));
+          break;
+        case "Popularity":
+          this.products.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case  "Price: Low to High":
+          this.products.sort((a, b) => a.price - b.price);
+          break;
+        case  "Price: High to Low":
+          this.products.sort((a, b) => b.price - a.price);
+          break;
       }
     },
   },
   methods: {
+    handleRangeChange(newRange) {
+      this.getProductList(
+        1,
+        this.search,
+        this.category,
+        newRange[0],
+        newRange[1]
+      );
+    },
     selectCategory(selectedItems) {
       let selectCategory;
       const selectedItem = selectedItems[selectedItems.length - 1];
@@ -259,22 +286,29 @@ export default {
           break;
         }
       }
-      console.log(selectCategory.name);
-      if (selectCategory) {
 
-          const filteredProducts = this.products.filter(product => product.category === selectCategory.name);
-          this.products = filteredProducts;
+      if (selectCategory) {
+        this.getProductList(1, this.search, selectCategory.name);
       } else {
-          console.log("No category selected.");
-          // Handle the case where no category is selected
+        console.log("No category selected.");
+        // Handle the case where no category is selected
       }
     },
-    getProductList(pageNumber, search) {
-      if (search === undefined || pageNumber === undefined) {
+    getProductList(pageNumber, search, category, minPrice, maxPrice) {
+      if (search === undefined) {
         search = "";
       }
+      if (category === undefined) {
+        category = "";
+      }
+      if (minPrice === undefined) {
+        minPrice = 0;
+      }
+      if (maxPrice === undefined) {
+        maxPrice = 100000;
+      }
 
-      let link = `product/list?page=${pageNumber}&search=${search}`;
+      let link = `product/list?page=${pageNumber}&search=${search}&category=${category}&minPrice=${minPrice}&maxPrice=${maxPrice}`;
       this.$store
         .dispatch("requestGet", link)
         .then((response) => {
@@ -282,6 +316,11 @@ export default {
             this.products = response.data.products;
             this.pages = response.data.total_pages;
             this.totalPoruducts = response.data.total;
+            this.min = response.data.min_price;
+            this.max = response.data.max_price;
+            let { minPrice, maxPrice } = this.findMinMaxPrice(this.products);
+            this.range[0] = minPrice;
+            this.range[1] = maxPrice;
           }
         })
         .catch((error) => {
@@ -289,7 +328,23 @@ export default {
         });
     },
     next(page) {
-      this.getProductList(page);
+      this.getProductList(page, this.search, this.category);
+    },
+    findMinMaxPrice(products) {
+      let minPrice = Number.POSITIVE_INFINITY;
+      let maxPrice = Number.NEGATIVE_INFINITY;
+
+      for (let i = 0; i < products.length; i++) {
+        let price = products[i].price;
+        if (price < minPrice) {
+          minPrice = price;
+        }
+        if (price > maxPrice) {
+          maxPrice = price;
+        }
+      }
+
+      return { minPrice, maxPrice };
     },
   },
   created() {
